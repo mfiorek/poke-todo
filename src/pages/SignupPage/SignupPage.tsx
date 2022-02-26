9e4a6cbcdcc70e8471f525b9c09ba0be66e9359f
fior <marcin.lukasz.fiorek@gmail.com>
2022-02-26 23:38:03 +0100

    6 | Improve Login and Signup pages

--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -9,7 +9,7 @@ import FormInput from '../../components/FormInput/FormInput';
 const SignupPage: React.FC = () => {
   const { signup } = useAuth();
   const navigate = useNavigate();
-  
+
   const [nick, setNick] = useState('');
   const [nickError, setNickError] = useState('');
   const [email, setEmail] = useState('');
@@ -47,10 +47,9 @@ const SignupPage: React.FC = () => {
       setLoaing(true);
       signup(email, password)
         .then((userCredential) => {
-          return database.collection('users').doc(userCredential.user?.uid).set({ nick: nick });
+          return database.collection('users').doc(userCredential.user?.uid).set({ name: nick });
         })
         .then(() => {
-          // TODO save the name in redux state (?)
           navigate('/');
         })
         .catch((err) => {
@@ -61,8 +60,9 @@ const SignupPage: React.FC = () => {
   };
 
   return (
-    <div className='flex flex-col justify-center items-center h-full'>
-    <Card title='Signup' className='w-5/6 md:w-1/2 lg:w-1/3 xl:w-1/4'>
+    <div className='flex flex-col justify-start items-center h-full'>
+      <img src='../../../static/images/logo.png' alt='Poke-todo' className='m-4 h-20 object-contain' />
+      <Card title='Signup' className='w-5/6 md:w-1/2 lg:w-1/3 xl:w-1/4'>
         {generalError && <div className='my-1 px-2 py-3 border border-red-800 bg-red-200 rounded-md text-red-800'>{generalError}</div>}
         <form onSubmit={handleSignup}>
           <FormInput id='nick' value={nick} onChange={setNick} type='text' errorMessage={nickError} label='Nick' />
@@ -70,8 +70,14 @@ const SignupPage: React.FC = () => {
           <FormInput id='password' value={password} onChange={setPassword} type='password' errorMessage={passwordError} label='Password' />
           <FormInput id='passwordConfirm' value={passwordConfirm} onChange={setPasswordConfirm} type='password' errorMessage={passwordConfirmError} label='Confirm Password' />
 
-          <button type='submit' disabled={loading} className='p-2 text-xl border rounded-md border-green-800 bg-green-700 w-full text-white'>
-            Sign up!
+          <button type='submit' disabled={loading} className='p-2 text-xl border rounded-md border-green-800 bg-green-700 w-full text-white disabled:opacity-50'>
+            {loading ? (
+              <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 animate-spin m-auto' viewBox='0 0 24 24' stroke='currentColor' fill='currentColor'>
+                <path d='M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z' />
+              </svg>
+            ) : (
+              <>Sign up!</>
+            )}
           </button>
         </form>
       </Card>
