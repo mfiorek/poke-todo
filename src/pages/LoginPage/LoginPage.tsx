646e70cfde58f88e5585320fcd9ac4774db8dd4d
Marcin Fiorek <59571041+mfiorek@users.noreply.github.com>
2022-02-26 23:39:21 +0100

    6 | Improve Login and Signup pages (#8)

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -44,14 +44,21 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className='flex flex-col justify-center items-center h-full'>
+    <div className='flex flex-col justify-start items-center h-full'>
+      <img src='../../../static/images/logo.png' alt='Poke-todo' className='m-4 h-20 object-contain' />
       <Card title='Login' className='w-5/6 md:w-1/2 lg:w-1/3 xl:w-1/4'>
         {generalError && <div className='my-2 px-2 py-3 border border-red-800 bg-red-200 rounded-md text-red-800'>{generalError}</div>}
         <form onSubmit={handleLogin}>
           <FormInput id='email' value={email} onChange={setEmail} type='email' errorMessage={emailError} label='Email' />
           <FormInput id='password' value={password} onChange={setPassword} type='password' errorMessage={passwordError} label='Password' />
-          <button type='submit' disabled={loading} className='p-2 text-xl border rounded-md border-green-800 bg-green-700 w-full text-white'>
-            Log In!
+          <button type='submit' disabled={loading} className='p-2 text-xl border rounded-md border-green-800 bg-green-700 w-full text-white disabled:opacity-50'>
+            {loading ? (
+              <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 animate-spin m-auto' viewBox='0 0 24 24' stroke='currentColor' fill='currentColor'>
+                <path d='M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z' />
+              </svg>
+            ) : (
+              <>Log In!</>
+            )}
           </button>
         </form>
       </Card>
