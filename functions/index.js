495ccbfab6538381e8fc51602d361d88369e0369
fior <marcin.lukasz.fiorek@gmail.com>
2022-03-27 23:32:43 +0200

    Add Firestore Cloud Function to make user an admin

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,79 @@
-const functions = require("firebase-functions");
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors')({ origin: true });
+const app = express();
+
+const validateFirebaseIdToken = async (req, res, next) => {
+  functions.logger.log('Check if request is authorized with Firebase ID token');
+
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) && !(req.cookies && req.cookies.__session)) {
+    functions.logger.error(
+      'No Firebase ID token was passed as a Bearer token in the Authorization header.',
+      'Make sure you authorize your request by providing the following HTTP header:',
+      'Authorization: Bearer <Firebase ID Token>',
+      'or by passing a "__session" cookie.',
+    );
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    functions.logger.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else if (req.cookies) {
+    functions.logger.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    functions.logger.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    next();
+    return;
+  } catch (error) {
+    functions.logger.error('Error while verifying Firebase ID token:', error);
+    res.status(403).send('Unauthorized');
+    return;
+  }
+};
+
+app.use(cors);
+app.use(cookieParser);
+app.use(validateFirebaseIdToken);
+app.post('/makeAdmin', (req, res) => {
+  admin
+    .auth()
+    .getUserByEmail(req.user.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, { isAdmin: true });
+    })
+    .then(() => {
+      res.send(`Success! ${req.user.email} has been added to admins`);
+    });
+});
+app.post('/removeAdmin', (req, res) => {
+  admin
+    .auth()
+    .getUserByEmail(req.user.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, { isAdmin: false });
+    })
+    .then(() => {
+      res.send(`Success! ${req.user.email} has been removed from admins`);
+    });
+});
+
+exports.app = functions.https.onRequest(app);
